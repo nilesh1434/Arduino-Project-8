f1c3217e3c81bc5a30fcdd5d109e9fc79b5c54c7
Nilesh Jain <79932578+nilesh1434@users.noreply.github.com>
2024-10-27 17:15:07 +0530

    Add files via upload

---
+++ njain36lab8.ino
@@ -0,0 +1,17 @@
+// Lab 7 - Graphing sensor data on a PC
+// This code is supposed to show the changes in the graph for a Photoresistor and a Potentiometer on the same graph.
+// Youtube link - https://youtu.be/IgZgEvdhBwM
+
+void setup() {
+  // initialize the serial communication:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // send the value of analog input 0 (Potentiometer):
+  Serial.println(analogRead(A0));
+  // send the value of analog input 1 (Photoresistor):
+  Serial.println(analogRead(A1));
+  // delay for 2 milliseconds
+  delay(2);
+}
